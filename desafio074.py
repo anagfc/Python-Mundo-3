cb214164371ce5b1a6f74f74f435501f7cf37e4a
anagfc <116309863+anagfc@users.noreply.github.com>
2023-02-18 15:14:35 -0300

    Desafio 74

---
+++ desafio074.py
@@ -0,0 +1,12 @@
+from random import randint
+números = (randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9))
+print(f'Sua tupla: {números}')
+maior = números[0]
+menor = números[0]
+for item in range(0, len(números)):
+    if números[item] < menor:
+        menor = números[item]
+    if números[item] > maior:
+        maior = números[item]
+print(f'Maior número da sua tupla: {maior}')
+print(f'Menor número da sua tupla: {menor}')
