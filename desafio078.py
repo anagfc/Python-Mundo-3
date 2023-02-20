248cfda7723f3a5a54c0f660cd5cc3671f6f2418
anagfc <116309863+anagfc@users.noreply.github.com>
2023-02-20 16:14:50 -0300

    Desafio 78

---
+++ desafio078.py
@@ -0,0 +1,4 @@
+números = [int(input('Insira um valor: ')), int(input('Insira mais um valor: ')), int(input('Insira o próximo valor: ')), int(input('Insira outro valor: ')), int(input('Insira o último valor: '))]
+print(f'A lista inserida foi {números}')
+print(f'Maior número da lista: {max(números)}.')
+print(f'Menor número da lista: {min(números)}.')
