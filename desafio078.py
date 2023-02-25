af065fe9b0747cb875efd40be3cdc261bab19490
anagfc <116309863+anagfc@users.noreply.github.com>
2023-02-25 17:16:56 -0300

    melhoria desafio 78

--- desafio078.py
+++ desafio078.py
@@ -1,4 +1,10 @@
 números = [int(input('Insira um valor: ')), int(input('Insira mais um valor: ')), int(input('Insira o próximo valor: ')), int(input('Insira outro valor: ')), int(input('Insira o último valor: '))]
 print(f'A lista inserida foi {números}')
-print(f'Maior número da lista: {max(números)}.')
-print(f'Menor número da lista: {min(números)}.')
+print(f'Maior número da lista: {max(números)}, na(s) posição(ões): ', end ='')
+for pos, item in enumerate(números):
+    if item == max(números):
+        print(pos, end='. ')
+print(f'\nMenor número da lista: {min(números)}, na(s) posição(ões): ', end='')
+for pos, item in enumerate(números):
+    if item == min(números):
+        print(pos, end='. ')
